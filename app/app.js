7df91c1fe6d54dde10090631aaff223b0f2d1a14
Bryan Finlayson <bdfinlayson@gmail.com>
2015-03-10 05:27:35 -0500

    add home module to app.js file and set default route to the home view

--- app/app.js
+++ app/app.js
@@ -1,7 +1,14 @@
 'use strict';
 
 angular.module('myApp', [
-  'ngRoute'
+  'ngRoute',
+  'myApp.home'  //newly addded home module
 ]).config(['$routeProvider', function($routeProvider) {
   //routes will be here
+  //set default view of app to home
+  $routeProvider.otherwise({
+    redirectTo: '/home'
+  });
 }]);
+
+
