6b9c21beb65a0922b828aca74cd4d4f699c2ee7e
Bryan Finlayson <bdfinlayson@gmail.com>
2015-03-14 13:54:02 -0500

    add auth controller

---
+++ js/auth/auth.controller.js
@@ -0,0 +1,48 @@
+angular
+  .module('annecdotals')
+  .controller('AuthController', AuthController);
+
+function AuthController($rootScope, $scope, $location, authFactory, BASE_URL) {
+	'use strict';
+
+	var vm = this;
+
+	vm.user = {};
+
+  vm.login = function () {
+    authFactory.login(vm.user, function (err, authData) {
+      if (err) {
+        console.log('Error logging in user:', err);
+      } else {
+        console.log('Logged in successfully', authData);
+        $rootScope.user = authData;
+        $location.path('/tas');
+        $scope.$apply();
+      }
+    });
+  };
+
+  vm.register = function () {
+    authFactory.register(vm.user, function (err, authData) {
+      if (err && err.code === 'EMAIL_TAKEN') {
+        console.log('Error creating user:', err);
+        vm.login();
+      } else if (err) {
+        console.log('Error creating user:', err);
+      } else {
+        console.log('User created successfully', authData);
+        vm.login();
+      }
+    });
+  };
+
+  vm.forgotPassword = function () {
+    authFactory.forgotPassword(vm.user, function (err) {
+      if (err) {
+        console.log('Error resetting password:', err);
+      } else {
+        console.log('Password reset email sent successfully');
+      }
+    });
+  };
+}
