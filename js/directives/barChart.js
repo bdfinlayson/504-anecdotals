f86ac4ee9e589967e5dff0a05234005149cb5c2f
Bryan Finlayson <bdfinlayson@gmail.com>
2015-03-22 13:39:55 -0500

    add code to get student test data and display in bar chart using d3

--- js/directives/barChart.js
+++ js/directives/barChart.js
@@ -8,24 +8,33 @@ angular
         data: '='
       },
       templateUrl: 'js/templates/bar-chart.html',
-      controller: function($scope) {
+      controller: function($scope, $location, studentFactory) {
 
-        var data = [4, 8, 15, 16, 23, 42];
+        var data = [];
 
-        var x = d3.scale.linear()
-          .domain([0, d3.max(data)])
-          .range([0, 420]);
+        studentFactory.findAll(function (students) {
+          console.log('from the student controller', students);
+          $.each(students, function(key,value) {
+              var arr = [];
+              console.log(value.times);
+              data = value.times;
+            });
+            var x = d3.scale.linear()
+              .domain([0, d3.max(data)])
+              .range([0, 420]);
 
-        d3.select(".chart")
-          .selectAll("div")
-          .data(data)
-          .enter().append("div")
-          .style("width", function(d) {
-            return x(d) + "px";
-          })
-          .text(function(d) {
-            return d;
+            d3.select(".chart")
+              .selectAll("div")
+              .data(data)
+              .enter().append("div")
+              .style("width", function(d) {
+                return x(d) + "px";
+              })
+              .text(function(d) {
+                return d;
+              });
           });
+
       }
     };
   });
