fcace56bf10dde93571d3f7b15221c3eb54ec4be
Bryan Finlayson <bdfinlayson@gmail.com>
2015-03-25 18:53:24 -0500

    add function to parse student test result data for bar chart

--- js/directives/barChart.js
+++ js/directives/barChart.js
@@ -1,28 +1,64 @@
 angular
   .module('anecdotals.directives.barChart', [])
   .directive('barChart', function() {
-      'use strict';
-      return {
-        restrict: 'EA',
-        scope: {
-          data: '='
-        },
-        templateUrl: 'js/templates/bar-chart.html',
-        controller: function($scope, $location, studentFactory, infoFactory) {
+    'use strict';
+    return {
+      restrict: 'EA',
+      scope: {
+        data: '='
+      },
+      templateUrl: 'js/templates/bar-chart.html',
+      controller: function($scope, $location, studentFactory, infoFactory) {
 
-          var data = [];
+        var info = [];
 
-          infoFactory.findOneStudent(function(students) {
-            // console.log('from the student controller', students);
-            $.each(students, function(key, value) {
-              // console.log(value.times);
-              data = value.times;
-              data.sort();
-              // console.log(data);
-            }); //closes $.each
-          });
+        var testName = [];
+        var timeTaken = [];
+        var standardTime = [];
+
+        infoFactory.findOneStudent(function(students) {
+          console.log('from the infoFactory findOneStudent function', students.testTimes);
+          $.each(students.testTimes, function(key, value) {
+            // console.log(value.times);
+            info = value;
+            console.log(info.timeTaken);
+            console.log(info.testName);
+            console.log(info.standardTime);
+
+            testName.push(info.testName);
+            timeTaken.push(info.timeTaken);
+            standardTime.push(info.standardTime);
+
+            console.log(testName, timeTaken, standardTime);
 
 
-              }
-            };
-          });
+
+
+        console.log(testName, timeTaken, standardTime);
+
+        var data = {
+          labels: [
+            'resilience', 'maintainability', 'accessibility',
+            'uptime', 'functionality', 'impact'
+          ],
+          series: [{
+            label: 'Time Taken',
+            values: timeTaken
+          }, {
+            label: 'Standard Time',
+            values: standardTime
+          }, {
+            label: '2014',
+            values: [31, 28, 14, 8, 15, 21]
+          }, ]
+        };
+
+
+      }); 
+    });
+
+
+
+      }
+    };
+  });
