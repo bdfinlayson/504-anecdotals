73601032521219cb19cbb4c6f65890bdf6b388fc
Bryan Finlayson <bdfinlayson@gmail.com>
2015-03-25 19:37:55 -0500

    add code to display student test performance data to page in bar chart. Charts are unique per student.

--- js/factories/info.factory.js
+++ js/factories/info.factory.js
@@ -58,5 +58,65 @@ function infoFactory($http, BASE_URL, $location) {
           console.log(data);
         });
     },
+    findStudentTests: function(cb) {
+      console.log('infoFactory function findOneStudent fired!');
+      var fb = new Firebase('https://504-anecdotals.firebaseio.com');
+      var user = fb.getAuth();
+      console.log(user);
+      var path = $location.$$path;
+      var pathId = path.slice(15);
+      console.log(pathId);
+
+      $http
+        .get(BASE_URL + '/teachers/' + user.uid + '/students/' + pathId + '.json')
+        .success(function(data) {
+
+          var info = [];
+
+          var testName = [];
+          var timeTaken = [];
+          var standardTime = [];
+
+          console.log('from the infoFactory findOneStudent function', data.testTimes);
+          $.each(data.testTimes, function(key, value) {
+            // console.log(value.times);
+            data = value;
+            console.log(data.timeTaken);
+            console.log(data.testName);
+            console.log(data.standardTime);
+
+            testName.push(data.testName);
+            timeTaken.push(data.timeTaken);
+            standardTime.push(data.standardTime);
+
+            console.log(testName, timeTaken, standardTime);
+
+
+
+
+            console.log(testName, timeTaken, standardTime);
+
+
+
+            data = {
+              labels: testName,
+              series: [{
+                label: 'Time Taken',
+                values: timeTaken
+              }, {
+                label: 'Standard Time',
+                values: standardTime
+              } ]
+            };
+
+
+
+          // cb(data);
+          // console.log(data);
+        });
+        cb(data);
+        console.log(data);
+      });
+    }
   };
 }
